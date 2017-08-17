4ffa54599e0b844bb556766ad440c613c6dff35b
cklwblove <lwbhtml@163.com>
2017-08-17 14:32:50 +0800

    更新extract-text-webpack-plugin版本

--- template/build/config/index.js
+++ template/build/config/index.js
@@ -21,7 +21,7 @@ module.exports = {
   },
   dev: {
     env: require('./dev.env'),
-    port: 8080,
+    port: 4000,
     autoOpenBrowser: true,
     assetsSubDirectory: 'assets',
     assetsPublicPath: '/',
